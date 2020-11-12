747e9cea663c0a9100f756e72a51da790942f387
Manuel Florez <manuelflorezw@gmail.com>
2020-11-11 20:16:03 -0500

    configuración generar war

--- src/main/java/com/agenda/app/controllers/ActividadController.java
+++ src/main/java/com/agenda/app/controllers/ActividadController.java
@@ -1,8 +1,10 @@
 package com.agenda.app.controllers;
 
+import com.agenda.app.dao.ActividadDao;
 import com.agenda.app.dto.ResponseDto;
 import com.agenda.app.entity.Actividad;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -13,9 +15,12 @@ import java.util.List;
 @CrossOrigin("*")
 public class ActividadController {
 
+    @Autowired
+    private ActividadDao actividadDao;
+
     @GetMapping("/all")
     public ResponseDto<List<Actividad>> findAll() {
-        return null;
+        return ResponseDto.ok(actividadDao.findAll()) ;
     }
 
     @PostMapping("/create")
